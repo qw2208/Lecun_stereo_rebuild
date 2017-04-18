6e2bf37f88c27b1679512f157b80d9a9fd983164
qw2208 <wuqingwei503@gmail.com>
2017-04-17 21:29:47 -0400

    Post process of the disp image

---
+++ post_process.py
@@ -0,0 +1,15 @@
+import cv2
+from PIL import Image
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+test = cv2.imread('disp.png')
+# print test
+test = np.power(test, 0.2).astype(np.float)
+print test
+plt.imshow(test)
+plt.axis('off')
+plt.savefig('disp2.png')
